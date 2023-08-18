89b83132295136e6e51805f9ee45075e003f8583
nongfugengxia <nongfugengxia@163.com>
2023-08-18 14:37:38 +0800

    支持同时杀多个同名进程

--- kill_process.m
+++ kill_process.m
@@ -6,7 +6,8 @@ function [ ] = kill_process(ProcessName)
 
 % eg:'WINWORD.EXE' % 
 
-%% %获取所有进程信息
+% 获取所有进程信息
+% tasklist /fi "imagename eq cmd.exe"
 [~,cmdout] = system('tasklist');
 
 cmdout = split(cmdout,strcat(10));
@@ -16,9 +17,13 @@ if isempty(WINWORD)  % 进程不存在则不需要杀
 end
 
 %% %获取指定进程信息
-WINWORD = split( WINWORD,' ');
+% if numel(WINWORD) > 1  % 有超过一个进程
+for i = 1 : numel(WINWORD)
+    cur_process = WINWORD{i};
+    cur_process_info = split( cur_process,' ');
 
-%% % 杀死指定进程
-system(strcat('taskkill /pid' , 32 , WINWORD{ find( ismember( WINWORD, 'Console' ) , 1 ) - 1 } , 32 , ' /f' ) );
+    %% % 杀死指定进程
+    system(strcat('taskkill /pid' , 32 , cur_process_info{ find( ismember( cur_process_info, 'Console' ) , 1 ) - 1 } , 32 , ' /f' ) );
 
 end
+% end
