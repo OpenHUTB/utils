390b0dfdb13bedeafd9ec1e5d457e88a52f5f902
nongfugengxia <nongfugengxia@163.com>
2023-09-04 14:48:50 +0800

    更新matlab主目录

--- auto/auto_pull_launch.bat
+++ auto/auto_pull_launch.bat
@@ -1,4 +1,4 @@
-C:\BaiduSyncdisk\matlab\bin\matlab.exe -nodesktop -nosplash -r auto_pull_main
+D:\work\matlab\bin\matlab.exe -nodesktop -nosplash -r auto_pull_main
 :: com.mathworks.mde.desk.MLDesktop.getInstance.closeCommandWindow;  能在图形界面关闭命令行窗口，但是不能在-nodesktop模式下关闭命令行窗口
 :: -r auto_pull
 :: 暂时没有解决 MATLAB Command window 窗口的隐藏
