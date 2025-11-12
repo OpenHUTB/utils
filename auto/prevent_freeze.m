cb70ed610be4a2bc2e78ea8a9a3ad2c141e2258d
donghaiwang <whd@hutb.edu.cn>
2025-11-12 16:25:52 +0800

    2025培训

--- auto/prevent_freeze.m
+++ auto/prevent_freeze.m
@@ -1,3 +1,22 @@
+
+
+% 事业单位培训（沃培网：https://hn.wopeiwang.com/）
+
+init_robot
+
+wait_time = 2;
+while true
+    pause(wait_time);
+    robot.mouseMove(0.4*width, 0.3*height);
+    pause(wait_time);
+    robot.mouseMove(0.5*width, 0.5*height);
+    pause(wait_time);
+    robot.mousePress  (java.awt.event.InputEvent.BUTTON1_MASK);
+    robot.mouseRelease(java.awt.event.InputEvent.BUTTON1_MASK);
+end
+
+%%
+%{
 % 湖南省高校师资培训避免看视频中断
 % 参考：https://blog.csdn.net/u011389706/article/details/57399942
 
@@ -13,25 +32,6 @@ while true
     robot.mousePress  (java.awt.event.InputEvent.BUTTON1_MASK);
     robot.mouseRelease(java.awt.event.InputEvent.BUTTON1_MASK);
 end
-
-
-
-%% Control mouse
-% single press
-
-% robot.mousePress(java.awt.event.InputEvent.BUTTON1_MASK);
-% robot.mouseRelease(java.awt.event.InputEvent.BUTTON1_MASK);
-% 
-% % double press
-% figure(gcf);
-% drawnow;
-% robot.mousePress(java.awt.event.InputEvent.BUTTON1_MASK);
-% robot.mouseRelease(java.awt.event.InputEvent.BUTTON1_MASK);
-% robot.mousePress(java.awt.event.InputEvent.BUTTON1_MASK);
-% robot.mouseRelease(java.awt.event.InputEvent.BUTTON1_MASK);
-% 
-% %% Control Keyboard 
-% robot.keyPress(java.awt.event.KeyEvent.VK_CONTROL);
-% robot.keyPress(java.awt.event.KeyEvent.VK_P);
+%}
 
 
